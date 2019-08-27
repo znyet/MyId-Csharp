feb02195c62609256600abe9c04fb0f28f45bfb5
叶涛 <821023977@qq.com>
2019-08-27 10:00:29 +0800

    1

--- MyIdClient/MyIdPooled.cs
+++ MyIdClient/MyIdPooled.cs
@@ -38,6 +38,7 @@ namespace MyIdClient
                 {
                     IChannelPipeline pip = channel.Pipeline;
                     pip.AddLast(echoHandler);
+					
                 }));
 
         }
